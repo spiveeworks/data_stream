e55594fff73afe373ae61bef5b5c014691616631
SpiveeWorks <spiveehere@gmail.com>
2017-06-11 12:45:02 +1000

    separate impls

---
+++ src/stream_container/impls.rs
@@ -0,0 +1,25 @@
+
+impl StreamContainer<u8> for [u8; 8]
+{
+    type Iter = std::vec::IntoIter<u8>;
+    fn fill_with<I: Iterator<Item = u8>> (stream: &mut I) -> Option<Self>
+    {
+        let mut out = [0; 8];
+        for x in &mut out
+        {
+            if let Some(thing) = stream.next()
+            {
+                *x = thing;
+            }
+            else
+            {
+                return None;
+            }
+        }
+        Some(out)
+    }
+    fn into_stream(self) -> Self::Iter
+      {self.to_vec().into_iter()}
+}
+
+
