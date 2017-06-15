9a54730463689dc5d90598119670073da5c896ab
SpiveeWorks <spiveehere@gmail.com>
2017-06-16 08:31:55 +1000

    rough array impl
    not yet tested

--- src/stream_container/impls.rs
+++ src/stream_container/impls.rs
@@ -2,27 +2,64 @@ use stream_container::{StreamContainer};
 
 use std::vec;
 
-impl StreamContainer<u8> for [u8; 8]
+macro_rules! try_option 
 {
-    type Iter = vec::IntoIter<u8>;
-    fn fill_with<I: Iterator<Item = u8>> (stream: &mut I) -> Option<Self>
+    {$x:expr} => 
     {
-        let mut out = [0; 8];
-        for x in &mut out
+        match $x
         {
-            if let Some(thing) = stream.next()
-            {
-                *x = thing;
-            }
-            else
-            {
-                return None;
-            }
+            Some(x) => x,
+            None => return None,
         }
-        Some(out)
-    }
-    fn into_stream(self) -> Self::Iter
-      {self.to_vec().into_iter()}
+    };
 }
 
+macro_rules! dummy 
+{
+    ($x:expr) => ();
+}
 
+macro_rules! expr_arr
+{
+    [$x: expr; $($ns:tt),*] => {[$({dummy!($ns); $x})]};
+}
+
+macro_rules! array_impl_stream_container 
+{
+    {$n:expr $($ns:expr)+} => 
+    {
+        impl<T> StreamContainer<T> for [T; $n]
+        {
+            type Iter = vec::IntoIter<T>;
+            fn fill_with<I: Iterator<Item = T>> (stream: &mut I) -> Option<Self>
+            {
+                Some(expr_arr![
+                     try_option!(stream.next()); 
+                     $($ns),+])
+            }
+            fn into_stream(self) -> Self::Iter
+              {self.to_vec().into_iter()}
+        }
+
+        array_impl_stream_container!{$($ns)+}
+    };
+
+    {$n:expr} => {
+        impl<T> StreamContainer<T> for [T; $n]
+        {
+            type Iter = vec::IntoIter<T>;
+            fn fill_with<I: Iterator<Item = T>> (stream: &mut I) -> Option<Self>
+              {Some([])}
+            fn into_stream(self) -> Self::Iter
+              {self.to_vec().into_iter()}
+        }
+    };
+}
+
+array_impl_stream_container!
+{
+                         32 31 30
+    29 28 27 26 25 24 23 22 21 20
+    19 18 17 16 15 14 13 12 11 10
+     9  8  7  6  5  4  3  2  1  0
+}
